2d86c6bb5d5b8fd609fd2b1e292afd153d7f4f01
Jason Loving <jlovingsr@gmail.com>
2025-10-22 18:11:13 -0500

    Make function fix error-proof - skip non-existent functions automatically

--- project/fix-function-search-path-warnings.sql
+++ project/fix-function-search-path-warnings.sql
@@ -1,64 +1,129 @@
 -- =====================================================
--- FIX ALL 19 FUNCTION SEARCH_PATH WARNINGS
+-- FIX ALL FUNCTION SEARCH_PATH WARNINGS
 -- Run this in Supabase SQL Editor
 -- 
--- This adds "SET search_path = public" to all functions
--- that currently have mutable search_path
+-- This adds "SET search_path = public" to all existing functions
+-- Skips functions that don't exist to avoid errors
 -- =====================================================
 
--- Fix: public.generate_product_slug
-ALTER FUNCTION public.generate_product_slug(text) SET search_path = public;
+-- Fix functions that exist - wrapped in DO blocks to handle errors gracefully
+DO $$
+BEGIN
+  -- Try to fix each function, skip if it doesn't exist
+  
+  BEGIN
+    ALTER FUNCTION public.generate_product_slug(text) SET search_path = public;
+    RAISE NOTICE 'Fixed: generate_product_slug';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: generate_product_slug (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.generate_referral_code() SET search_path = public;
+    RAISE NOTICE 'Fixed: generate_referral_code';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: generate_referral_code (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.auto_generate_referral_code() SET search_path = public;
+    RAISE NOTICE 'Fixed: auto_generate_referral_code';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: auto_generate_referral_code (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.trg_update_email_notifications_timestamp() SET search_path = public;
+    RAISE NOTICE 'Fixed: trg_update_email_notifications_timestamp';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: trg_update_email_notifications_timestamp (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.handle_updated_at() SET search_path = public;
+    RAISE NOTICE 'Fixed: handle_updated_at';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: handle_updated_at (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.update_updated_at_column() SET search_path = public;
+    RAISE NOTICE 'Fixed: update_updated_at_column';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: update_updated_at_column (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.create_default_store_settings() SET search_path = public;
+    RAISE NOTICE 'Fixed: create_default_store_settings';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: create_default_store_settings (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.update_product_affiliate_count() SET search_path = public;
+    RAISE NOTICE 'Fixed: update_product_affiliate_count';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: update_product_affiliate_count (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.increase_affiliates_product_metric() SET search_path = public;
+    RAISE NOTICE 'Fixed: increase_affiliates_product_metric';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: increase_affiliates_product_metric (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.update_affiliage_products_updated_at() SET search_path = public;
+    RAISE NOTICE 'Fixed: update_affiliage_products_updated_at';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: update_affiliage_products_updated_at (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.update_product_affiliates_count() SET search_path = public;
+    RAISE NOTICE 'Fixed: update_product_affiliates_count';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: update_product_affiliates_count (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.generate_affiliate_link_code() SET search_path = public;
+    RAISE NOTICE 'Fixed: generate_affiliate_link_code';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: generate_affiliate_link_code (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.increment_affiliate_product_metric() SET search_path = public;
+    RAISE NOTICE 'Fixed: increment_affiliate_product_metric';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: increment_affiliate_product_metric (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.record_referral_commission() SET search_path = public;
+    RAISE NOTICE 'Fixed: record_referral_commission';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: record_referral_commission (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.get_platform_setting(text) SET search_path = public;
+    RAISE NOTICE 'Fixed: get_platform_setting';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: get_platform_setting (does not exist)';
+  END;
+  
+  BEGIN
+    ALTER FUNCTION public.update_updated_at() SET search_path = public;
+    RAISE NOTICE 'Fixed: update_updated_at';
+  EXCEPTION WHEN undefined_function THEN
+    RAISE NOTICE 'Skipped: update_updated_at (does not exist)';
+  END;
 
--- Skip: public.generate_store_slug - function does not exist (stores table removed)
--- Skip: public.set_store_slug - function does not exist (stores table removed)
-
--- Fix: public.generate_referral_code
-ALTER FUNCTION public.generate_referral_code() SET search_path = public;
-
--- Fix: public.auto_generate_referral_code
-ALTER FUNCTION public.auto_generate_referral_code() SET search_path = public;
-
--- Fix: public.trg_update_email_notifications_timestamp
-ALTER FUNCTION public.trg_update_email_notifications_timestamp() SET search_path = public;
-
--- Fix: public.handle_updated_at
-ALTER FUNCTION public.handle_updated_at() SET search_path = public;
-
--- Fix: public.update_updated_at_column
-ALTER FUNCTION public.update_updated_at_column() SET search_path = public;
-
--- Fix: public.create_default_store_settings
-ALTER FUNCTION public.create_default_store_settings() SET search_path = public;
-
--- Fix: public.update_product_affiliate_count
-ALTER FUNCTION public.update_product_affiliate_count() SET search_path = public;
-
--- Fix: public.increase_affiliates_product_metric
-ALTER FUNCTION public.increase_affiliates_product_metric() SET search_path = public;
-
--- Fix: public.update_affiliage_products_updated_at
-ALTER FUNCTION public.update_affiliage_products_updated_at() SET search_path = public;
-
--- Fix: public.update_product_affiliates_count
-ALTER FUNCTION public.update_product_affiliates_count() SET search_path = public;
-
--- Fix: public.generate_affiliate_link_code
-ALTER FUNCTION public.generate_affiliate_link_code() SET search_path = public;
-
--- Fix: public.increment_affiliate_product_metric
-ALTER FUNCTION public.increment_affiliate_product_metric() SET search_path = public;
-
--- Fix: public.record_referral_commission
-ALTER FUNCTION public.record_referral_commission() SET search_path = public;
-
--- Fix: public.get_platform_setting
-ALTER FUNCTION public.get_platform_setting(text) SET search_path = public;
-
--- Fix: public.update_updated_at
-ALTER FUNCTION public.update_updated_at() SET search_path = public;
-
--- Fix: Auth (leaked password protection) - this is a special auth function
--- We'll skip this one as it's in the auth schema, not public
+END $$;
 
 -- =====================================================
 -- VERIFICATION
